e1114acca0f6a84c934bc1f1731ea88347f34901
cymmGithub <47364212+cymmGithub@users.noreply.github.com>
2022-06-30 21:27:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ import { determineWhoWins, rectangularCollision, soundEffects} from "./js/utils.
 
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-
-
-
-
-
-
-
 canvas.width = 1024;
 canvas.height = 576;
 
